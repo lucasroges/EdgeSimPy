17d2b023facefcc1cb6b12c585f64a02e019b5ed
Lucas Roges <lucasrogesaraujo@gmail.com>
2023-06-15 18:29:18 -0300

    [Fix] Export polynomial server power model

--- edge_sim_py/components/power_models/servers/__init__.py
+++ edge_sim_py/components/power_models/servers/__init__.py
@@ -2,6 +2,7 @@
 __version__ = "1.1.0"
 
 # Server power models
+from .polynomial_server_power_model import PolynomialServerPowerModel
 from .linear_server_power_model import LinearServerPowerModel
 from .square_server_power_model import SquareServerPowerModel
 from .cubic_server_power_model import CubicServerPowerModel
